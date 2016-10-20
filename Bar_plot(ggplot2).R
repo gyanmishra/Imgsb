7eefd2e2a755afb2c3af200ece4d008a4924f5f6
gyanmishra <j12mishra@gmail.com>
2016-10-20 17:50:52 +0530

    Update Bar_plot(ggplot2).R

--- Bar_plot(ggplot2).R
+++ Bar_plot(ggplot2).R
@@ -1,11 +1,18 @@
-a <- data.frame(replicate(2,sample(0:100,5,rep=TRUE)))
-b <- letters[1:5]
-dat <- data.frame(b,a)
+a <- data.frame(replicate(2,sample(0:100,5,rep=TRUE))) #####
+b <- letters[1:5]                                      #####   create data frame     
+dat <- data.frame(b,a)                                 #####
+
+#     b X1 X2
+#     a 60 33
+#     b 58 35
+#     c 65 51
+#     d 38 56
+#     e 66 10
 
 
 library(ggplot2)
 library(reshape)
-dat <- read.csv("NCoR1_overlap_wd_other_TF_bar_plot.txt", header = T,sep = "\t")
+
 dfm <- melt(dat)
 ggplot(dfm,aes(x = b,y = value)) + 
 geom_bar(position="dodge",stat="identity",aes(fill=variable)) + 
@@ -17,4 +24,4 @@ theme(axis.text.x=element_text(size=16,face="bold")
   , legend.title = element_text(colour="black", size=15, face="bold")) +
 labs(x = "sample",y="percent",label =FALSE) +
 guides(fill=guide_legend(title="Variable",label.theme = element_text(angle = 0,size = 20))) +
-ggsave("NCoR1_overlap_wd_other_TF_bar_plot.jpeg", width = 20, height = 20, units = "cm", dpi =1000)
+ggsave("bar_plot.jpeg", width = 20, height = 20, units = "cm", dpi =1000)
