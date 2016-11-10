96202b3d03ff80bb35f2dfb768342b2e9feb48af
gyanmishra <j12mishra@gmail.com>
2016-11-10 11:36:11 +0530

    Update Bar_plot(ggplot2).R

--- Bar_plot(ggplot2).R
+++ Bar_plot(ggplot2).R
@@ -17,11 +17,11 @@ dfm <- melt(dat)
 ggplot(dfm,aes(x = b,y = value)) + 
 geom_bar(position="dodge",stat="identity",aes(fill=variable)) + 
 theme(axis.text.x=element_text(size=16,face="bold")
-  , axis.title.x=element_text(size=18)
-  , axis.text.y=element_text(size=16,face = "bold")
-  , axis.title.y=element_text(size=18)
-  , plot.title=element_text("Uns", size=20, face="bold", color="darkgreen")
-  , legend.title = element_text(colour="black", size=15, face="bold")) +
+    , axis.title.x=element_text(size=18)
+    , axis.text.y=element_text(size=16,face = "bold")
+    , axis.title.y=element_text(size=18)
+    , plot.title=element_text("Uns", size=20, face="bold", color="darkgreen")
+    , legend.title = element_text(colour="black", size=15, face="bold")) +
 labs(x = "sample",y="percent",label =FALSE) +
 guides(fill=guide_legend(title="Variable",label.theme = element_text(angle = 0,size = 20))) +
 ggsave("bar_plot.jpeg", width = 20, height = 20, units = "cm", dpi =1000)
